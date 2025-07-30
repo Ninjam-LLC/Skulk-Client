45f1f499e7381da17e490e7bad353c1c39daef81
Aries Powvalla <aries@ninjam.co>
2025-07-29 20:16:38 -0700

    stash AA

---
+++ src/client/java/com/ariesninja/skulkpk/client/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,16 @@
+package com.ariesninja.skulkpk.client.mixin;
+
+import com.ariesninja.skulkpk.client.core.PlayerController;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+    }
+}
